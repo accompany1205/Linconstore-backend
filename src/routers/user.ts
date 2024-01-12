5260e3f59feb3fd6169f4007bfb0ac30d3d39c58
accompany1205 <accompaniedwithyou@gmail.com>
2024-01-12 02:26:52 -0800

    handled api error

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -719,70 +719,70 @@ router.post("/user/rate", auth, async (req: Request, res: Response) => {
     res.status(401).send(e);
   }
 });
-router.post("/user/language", auth, async (req: Request, res: Response) => {
-  const data: myLanguage = {
-    language: req.body.language,
-  };
-  const name = req.user.firstName;
-  const { comment, rating: rating1, productId } = req.body;
-  try {
-    const rating: IRating | null = await Rating.findOne({
-      productId: new ObjectId(data.productId),
-    });
-    if (rating) {
-      const existingUser = rating.ratings.find(
-        (data) => data.userId.toHexString() === req.user._id.toHexString()
-      );
-      if (existingUser) {
-        return res
-          .status(401)
-          .send({ status: "User cant rate the same item more than once" });
-      }
-      rating.ratings = rating.ratings.concat({
-        rating: data.rating,
-        name: req.user.email,
-        userId: req.user._id,
-        comment: req.body.comment,
-      });
-      const newRating: number = rating.ratings.reduce(
-        (acc, obj) => acc + obj.rating,
-        0
-      );
-      const length: number = rating.ratings.length;
-      rating.averageRating = Number((newRating / length).toFixed(1));
-      await rating.save();
-      return res.status(200).send(rating);
-    }
-    const newData: newData = {
-      productId: req.body.productId,
-      ratings: [
-        {
-          userId: req.user._id,
-          rating: req.body.rating,
-          name,
-        },
-      ],
-      averageRating: data.rating,
-    };
-    const newRating = new Rating(newData);
-    await newRating.save();
-    const product: IProduct | null = await Product.findById(req.body.productId);
-    product!.ratingId = newRating._id;
-    product!.save();
-    const review = new Review({
-      description: comment,
-      rate: rating1,
-      productId,
-      name,
-      owner: product?.owner,
-    });
-    await review.save();
-    res.status(201).send(newRating);
-  } catch (e) {
-    console.log(e);
-    res.status(401).send(e);
-  }
-});
+// router.post("/user/language", auth, async (req: Request, res: Response) => {
+//   const data: myLanguage = {
+//     language: req.body.language,
+//   };
+//   const name = req.user.firstName;
+//   const { comment, rating: rating1, productId } = req.body;
+//   try {
+//     const rating: IRating | null = await Rating.findOne({
+//       productId: new ObjectId(data.productId),
+//     });
+//     if (rating) {
+//       const existingUser = rating.ratings.find(
+//         (data) => data.userId.toHexString() === req.user._id.toHexString()
+//       );
+//       if (existingUser) {
+//         return res
+//           .status(401)
+//           .send({ status: "User cant rate the same item more than once" });
+//       }
+//       rating.ratings = rating.ratings.concat({
+//         rating: data.rating,
+//         name: req.user.email,
+//         userId: req.user._id,
+//         comment: req.body.comment,
+//       });
+//       const newRating: number = rating.ratings.reduce(
+//         (acc, obj) => acc + obj.rating,
+//         0
+//       );
+//       const length: number = rating.ratings.length;
+//       rating.averageRating = Number((newRating / length).toFixed(1));
+//       await rating.save();
+//       return res.status(200).send(rating);
+//     }
+//     const newData: newData = {
+//       productId: req.body.productId,
+//       ratings: [
+//         {
+//           userId: req.user._id,
+//           rating: req.body.rating,
+//           name,
+//         },
+//       ],
+//       averageRating: data.rating,
+//     };
+//     const newRating = new Rating(newData);
+//     await newRating.save();
+//     const product: IProduct | null = await Product.findById(req.body.productId);
+//     product!.ratingId = newRating._id;
+//     product!.save();
+//     const review = new Review({
+//       description: comment,
+//       rate: rating1,
+//       productId,
+//       name,
+//       owner: product?.owner,
+//     });
+//     await review.save();
+//     res.status(201).send(newRating);
+//   } catch (e) {
+//     console.log(e);
+//     res.status(401).send(e);
+//   }
+// });
 // router.get('/user/rate', auth, async (req : Request, res : Response) => {
 //     try {
 //         const rating : IRating [] = await Rating.findOne({});
